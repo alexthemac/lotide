1e80155542fbae4d47e3c07ae98f96117e5fe50c
Alex MacLure <almaclure@gmail.com>
2021-07-19 21:07:45 +0000

    fixig eslint problem

--- tail.js
+++ tail.js
@@ -18,11 +18,10 @@ const tail = function(array) {
 
 // TEST CODE
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-
+//Test Case: Extra test cases
 console.log(tail([1])); //should return an empty array
-
 console.log(tail([])); //should return an empty array.
